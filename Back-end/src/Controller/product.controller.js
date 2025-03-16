4b10698e34a57ed70eb677c22dab65d2d67e9cbc
Talha Asif <98359013+TalhahAsif@users.noreply.github.com>
2025-03-16 08:08:15 +0500

    add product error solved

--- Back-end/src/Controller/product.controller.js
+++ Back-end/src/Controller/product.controller.js
@@ -3,13 +3,16 @@ import Product from "../Models/Product.model.js";
 export const addProduct = async (req, res) => {
   const { title, description, category, price, quantity, createdby } = req.body;
   try {
-    if (!title || !description || !category || !quantity || !price) {
-      return res.status(500).json({
-        message: "Please fill all the fields",
-      });
+    if (
+      !title ||
+      !description ||
+      !category ||
+      !quantity ||
+      !price ||
+      !createdby
+    ) {
+      return res.status(400).json({ message: "All fields are required" });
     }
-
-    console.log(title);
 
     const isExist = await Product.findOne({ title });
 
