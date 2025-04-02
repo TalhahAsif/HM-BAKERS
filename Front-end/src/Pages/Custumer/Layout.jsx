2f7db6b31e1ded9806ace2b5e241873cb25e74fc
Talha Asif <98359013+TalhahAsif@users.noreply.github.com>
2025-04-02 12:33:32 +0500

    navbar fixed

--- Front-end/src/Pages/Custumer/Layout.jsx
+++ Front-end/src/Pages/Custumer/Layout.jsx
@@ -16,8 +16,12 @@ const Layout = () => {
 
   return (
     <section className="bg-gradient-to-br from-purple-50 via-white to-blue-50">
-      <div className="sticky top-4 flex justify-center items-center">
-      <nav className={`z-50 flex items-center px-6 py-3 w-[75%] md:rounded-xl ${isMenuOpen ? "rounded-t-xl" : "rounded-xl"} backdrop-blur-xl shadow-xl bg-fuchsia-50/50`}> 
+      <div className="z-50 sticky top-4 flex justify-center items-center">
+        <nav
+          className={`relative flex items-center px-6 py-3 w-[75%] md:rounded-xl ${
+            isMenuOpen ? "rounded-t-xl" : "rounded-xl"
+          } backdrop-blur-xl shadow-xl bg-black/10`}
+        >
           <div className="flex items-center justify-between w-full gap-6">
             <button
               className="md:hidden text- "
@@ -27,15 +31,13 @@ const Layout = () => {
             </button>
             <img src={logo} className="w-12 h-12" alt="Logo" />
             <div
-              className={`absolute md:static top-full left-0 w-full md:w-auto md:flex items-center gap-5 p-4 md:p-0 rounded-b-lg transition-all duration-300 ease-in-out ${
-                isMenuOpen ? "block" : "hidden"
-              }`}
+              className={`hidden md:static top-full left-0 w-full md:w-auto md:flex items-center gap-5 p-4 md:p-0 rounded-b-lg transition-all duration-300 ease-in-out`}
             >
               {navLinks.map((data, index) => (
                 <Link
                   to={data.link}
                   key={index}
-                  className="block md:inline py-2 md:py-0 text-gray-900 hover:text-gray-900 dark:hover:text-gray-300"
+                  className="block md:inline py-2 md:py-0 text-gray-900 hover:text-gray-900 dark:hover:text-gray-300 "
                 >
                   {data.title}
                 </Link>
@@ -52,6 +54,25 @@ const Layout = () => {
             </div>
           </div>
         </nav>
+
+        {isMenuOpen && (
+          <div
+            className={`absolute block md:hidden top-full left-1/2 transform -translate-x-1/2 
+                w-[75%] md:w-auto items-center gap-5 p-4 md:p-0 
+                rounded-b-lg transition-all duration-300 ease-in-out 
+                backdrop-blur-xl shadow-xl bg-black/10 md:bg-transparent `}
+          >
+            {navLinks.map((data, index) => (
+              <Link
+                to={data.link}
+                key={index}
+                className="block md:inline py-2 md:py-0 text-gray-900 hover:text-gray-900 dark:hover:text-gray-300 "
+              >
+                {data.title}
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
 
       <div>
